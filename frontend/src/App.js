b66c3020802c047c187b8f1cbeca31f00c987271
andrelucasf <andre.lucas.lemos@hotmail.com>
2022-07-25 22:38:30 -0300

    Finalizando front e testando o jogo
    Co-authored-by: Lucas Medeiros <medeirosrosalucas@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,91 +1,88 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Check from "../src/assets/check.svg"
-import CheckFailed from "../src/assets/failed.svg"
 import Arrow from "../src/assets/arrow.svg"
+import senhas from "../src/utils/filtered_passwords.txt"
+import strongPasswordChecker from "../src/utils/strongPasswordChecker"
 
 const App = () => {
-  const [validLength, setValidLength] = useState(false);
-  const [hasNumber, setHasNumber] = useState(false);
-  const [upperCase, setUpperCase] = useState(false);
-  const [lowerCase, setLowerCase] = useState(false);
+  const [senha, setSenha] = useState("")
 
   const {
     register: registerCreate,
     handleSubmit: handleSubmitCreate,
   } = useForm();
 
-  const onSubmitCreate = async (data) => {
-    console.log(data);
+  const sortearSenhas = () => {
+    fetch(senhas)
+      .then(r => r.text())
+      .then(text => {
+        const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+        var temp = text.toString().split("\n")
+        var randomNum = random(0, temp.length)
+        setSenha(temp[randomNum])
+      });
   };
 
-  const handleBlur = async (event) => {
-    const password = event.currentTarget.value;
-    setValidLength(false);
-    if (password.length >= 8) {
-      setValidLength(true);
-    }
-    setHasNumber(false);
-    if ((/\d/).test(password) === true) {
-      setHasNumber(true);
-    }
-    setUpperCase(false);
-    if ((/[A-Z]/).test(password) === true) {
-      setUpperCase(true);
-    }
-    setLowerCase(false);
-    if ((/[a-z]/).test(password) === true) {
-      setLowerCase(true);
-    }
+  const onSubmit = async (data) => {
+    console.log(data);
+    const steps = strongPasswordChecker(senha)
+    console.log(steps);
+    if(steps == data.steps){
+      alert("Parabéns, você acertou!!");
+      window.location.reload(); 
+    } else
+      alert("Você errou, tente novamente!")
   };
+
+  useEffect(() => {
+    sortearSenhas();
+    console.log(senha);
+  }, []);
 
   return (
     <>
       <div className="password">
         <div className="password-title">
           <h1 className="password-h1">Torne a senha forte!</h1>
-          <h2 className="password-h2">Sorteamos uma senha dentre as senhas mais utilizadas por usuários. A sua missão é fazer modificações de forma que essa senha se torne mais forte!</h2>
+          <h2 className="password-h2">Sorteamos uma senha dentre as senhas mais utilizadas por usuários. A sua missão é pontuar quantas modificações necessárias para que essa senha se torne mais forte!</h2>
         </div>
-        <form className="password-form" onSubmit={handleSubmitCreate(onSubmitCreate)}>
+        <form className="password-form" onSubmit={handleSubmitCreate(onSubmit)}>
           <div className="password-form-error">
-            {/* <div>
-              <img src={validLength ? Check : CheckFailed} className="password-error-check" alt=">" />
-              <span className={validLength ? "password-form-check-txt" : "password-form-error-txt"}>Mínimo 8 caracteres</span>
+            <span className={"password-form-check-txt"}>Dicas:</span>
+            <div>
+              <img src={Check} className="password-error-check" alt=">" />
+              <span className={"password-form-check-txt"}>Não pode conter 3 caracteres repetidos em sequência</span>
             </div>
             <div>
-              <img src={hasNumber ? Check : CheckFailed} className="password-error-check" alt=">" />
-              <span className={hasNumber ? "password-form-check-txt" : "password-form-error-txt"}>Caractere numérico</span>
+              <img src={Check} className="password-error-check" alt=">" />
+              <span className={"password-form-check-txt"}>De 6 a 20 caracteres</span>
             </div>
             <div>
-              <img src={upperCase ? Check : CheckFailed} className="password-error-check" alt=">" />
-              <span className={upperCase ? "password-form-check-txt" : "password-form-error-txt"}>Caractere maiúsculo</span>
+              <img src={Check} className="password-error-check" alt=">" />
+              <span className={"password-form-check-txt"}>Caractere numérico</span>
             </div>
-
             <div>
-              <img src={lowerCase ? Check : CheckFailed} className="password-error-check" alt=">" />
-              <span className={lowerCase ? "password-form-check-txt" : "password-form-error-txt"}>Caractere minúsculo</span>
-            </div> */}
+              <img src={Check} className="password-error-check" alt=">" />
+              <span className={"password-form-check-txt"}>Caractere maiúsculo</span>
+            </div>
+            <div>
+              <img src={Check} className="password-error-check" alt=">" />
+              <span className={"password-form-check-txt"}>Caractere minúsculo</span>
+            </div>
+            <div>
+              <h1 className="password-form-check-txt">A senha sorteada foi:</h1>
+              <h1 className="password-select">{senha}</h1>
+            </div>
           </div>
-          <input type="text" placeholder="Modifique a senha" className="password-input"
-            {...registerCreate("password", {
-              onBlur: (e) => handleBlur(e),
+          <input type="text" placeholder="Modificações necessárias" className="password-input"
+            {...registerCreate("steps", {
               required: true,
-              minLength: {
-                value: 8,
-                message:
-                  "",
-              },
-              pattern: {
-                value:
-                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`!@#$%^&*()_+\-=\]{};':"\\|,.<>?~])[A-Za-z\d`!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]{8,}$/,
-                message:
-                  "",
-              },
             })}
           />
           <button className="password-btn-next" type="submit">
-            Confirmar
+            Confirmar!
             <img src={Arrow} className="password-btn-arrow" alt=">" />
           </button>
         </form>
